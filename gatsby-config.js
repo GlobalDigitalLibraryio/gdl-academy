98f92f6e021c070b81827c6cd392f72b7c7bed7d
Kien Vu <kienv91@gmail.com>
2019-04-23 13:04:33 +0200

    feat: added offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,7 @@ module.exports = {
         icon: `src/assets/favicon.png` // This path is relative to the root of the site.
       }
     },
+    'gatsby-plugin-offline',
     'gatsby-plugin-typescript'
   ]
 };
